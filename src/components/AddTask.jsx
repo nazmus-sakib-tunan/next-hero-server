c9c3eda4a3ebb14eba278a6cf1b2185e0a6242b2
Tunan10 <tunansakib200@gmail.com>
2026-05-26 00:56:18 +0600

    geting data to server site

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {CirclePlus} from "@gravity-ui/icons";
-import {Button, Input, Label, Modal, Surface, TextField} from "@heroui/react";
+import {Button, Input, Label, Modal, Surface, TextField, ListBox, Select} from "@heroui/react";
 
 export function AddTask({createATask}) {
   return (
@@ -21,7 +21,7 @@ export function AddTask({createATask}) {
             <Modal.Body className="p-6">
               <Surface variant="default">
                 <form action={createATask} className="flex flex-col gap-4">
-                  <TextField className="w-full" name="name" type="text" variant="secondary">
+                  <TextField className="w-full" name="title" type="text" variant="secondary">
                     <Label>Title</Label>
                     <Input placeholder="Enter your task title" />
                   </TextField>
@@ -29,17 +29,57 @@ export function AddTask({createATask}) {
                     <Label>task description</Label>
                     <Input placeholder="Enter your task description" />
                   </TextField>
-                  <TextField className="w-full" name="phone" type="tel" variant="secondary">
-                    <Label>Phone</Label>
-                    <Input placeholder="Enter your phone number" />
-                  </TextField>
-                  <TextField className="w-full" name="company" variant="secondary">
-                    <Label>Company</Label>
-                    <Input placeholder="Enter your company name" />
-                  </TextField>
-                  <TextField className="w-full" name="message" variant="secondary">
-                    <Label>Message</Label>
-                    <Input placeholder="Enter your message" />
+                <Select name="priority" className="w-[256px]" placeholder="Select one">
+      <Label>Priority</Label>
+      <Select.Trigger>
+        <Select.Value />
+        <Select.Indicator />
+      </Select.Trigger>
+      <Select.Popover>
+        <ListBox>
+          <ListBox.Item id="low" textValue="low">
+            low
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="medium" textValue="medium">
+            medium
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="high" textValue="high">
+            high
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+        </ListBox>
+      </Select.Popover>
+                  </Select>
+                  
+                  <Select name="status" className="w-[256px]" placeholder="Select one">
+      <Label>Status</Label>
+      <Select.Trigger>
+        <Select.Value />
+        <Select.Indicator />
+      </Select.Trigger>
+      <Select.Popover>
+        <ListBox>
+          <ListBox.Item id="pending" textValue="Pending">
+            Pending
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="progress" textValue="Progress">
+            In Progress
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+          <ListBox.Item id="completed" textValue="Completed">
+            Completed
+            <ListBox.ItemIndicator />
+          </ListBox.Item>
+        </ListBox>
+      </Select.Popover>
+    </Select>
+                  
+                  <TextField className="w-full" name="assignTo" variant="secondary">
+                    <Label>Assign to</Label>
+                    <Input placeholder="Enter the person's name" />
                   </TextField>
 
                 <Modal.Footer>
